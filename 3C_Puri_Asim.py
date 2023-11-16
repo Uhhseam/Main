77a09bc3cf86cd08a2de13efd962b5eec9915611
Uhhseam <asimpuri2012@gmail.com>
2023-11-15 20:15:23 -0500

    Update 3C_Puri_Asim.py

--- 3C_Puri_Asim.py
+++ 3C_Puri_Asim.py
@@ -4,25 +4,38 @@
 ## Date: 9/29/2023
 ##--------------------------------------------------------------
 
-flag1 = 'y'
-while flag1 == 'y': 
-    cost = 0
+def main():
+    flag = 'y'
+    while flag == 'y':
+        budget = tBudgetCall() 
+        cost = expenseCall()
+        tBudget = budget - cost
+        answer(tBudget,cost,budget)
+        flag = input('\nWant to enter another budget? (y/n)')
+
+def tBudgetCall():
     budget = float(input("Enter your budget for the month: $"))
-    flag2 = 'y'
-    while flag2 == 'y':
+    return(budget)
+
+def expenseCall():
+    cost = 0
+    flag = 'y'
+    while flag == 'y':
         expense = float(input('Enter you expense: '))
         cost += expense
-        flag2 = input('Would you like to enter another expense? (y/n)')
-    print(cost)
-    tBudget = budget - cost
+        flag = input('Would you like to enter another expense? (y/n)')
+    return(cost)
+
+def answer(tBudget,cost,budget):
     if tBudget == 0:
         print('You are at equilibrium. \n\nHere is your remaining budget for the month: $',format(tBudget,',.2f'),
-              '\nHere is your total cost for the month $',format(cost,',.2f'), sep='')
+            '\nHere is your total cost for the month $',format(cost,',.2f'), sep='')
     elif tBudget < 0 :
         print('You went over the budget.\n\nHere is your remaining budget for the month: $',format(tBudget,',.2f'),
-              '\nHere is your total cost for the month $',format(cost,',.2f'), sep='')
+            '\nHere is your total cost for the month $',format(cost,',.2f'), sep='')
     elif tBudget < budget:
         print('You are under the budget.\n\nHere is your remaining budget for the month: $',format(tBudget,',.2f'),
-              '\nHere is your total cost for the month $',format(cost,',.2f'), sep='')
-    flag1 = input('\nWant to enter another budget? (y/n)')
+            '\nHere is your total cost for the month $',format(cost,',.2f'), sep='')
+
+main()
 print('\n~~~~ Good By ~~~~\n')
