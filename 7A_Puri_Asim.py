1751e6568728c0fd07f7684a890c804d6d413af4
Uhhseam <asimpuri2012@gmail.com>
2023-11-17 17:02:15 -0500

    Update 7A_Puri_Asim.py

--- 7A_Puri_Asim.py
+++ 7A_Puri_Asim.py
@@ -1,22 +1,32 @@
 import random
-words = ("python", "jumble", "easy", "difficult", "answer", "xylophone",
+
+def jumbler():
+    words = ("python", "jumble", "easy", "difficult", "answer", "xylophone",
          "dexter", "christmas")
-word = random.choice(words)
-correct = word
+    word = random.choice(words)
+    correct = word
+    return(correct, word)
+
 jumble = ""
+again = 'y'
+while again.lower() == "y":
+    correct, word = jumbler()
+    while word:
+        position = random.randrange(len(word))
+        jumble += word[position]
+        word = word[:position] + word[position+1:]
 
-while word:
-    position = random.randrange(len(word))
-    jumble += word[position]
-    word = word[:position] + word[position+1:]
+    print("******** Welcome to Jumble Word ********")
+    print("\nUnscramble the word to win")
+    print("The jumbe is ",jumble)
+    guess = input("\nYour guess: ")
+    while guess != correct and guess !="":
+        print("\nSorry, that's not it\nPress enter to stop guessing\n")
+        guess = input("Your guess :")
+    if guess == correct:
+        print("That's correct!\n")
 
-print("******** Welcome to Jumble Word ********")
-print("\nUnscramble the word to win")
-print("The jumbe is ",jumble)
-guess = input("\nYour guess: ")
-while guess != correct and guess !="":
-    print("Sorry, that's not it")
-    guess = input("Your guess :")
-print("Thanks for playing")
-
-input("\n\nPress enter key to exit")
+    again = input('Would you like to play again? (y/n)\n >>> ')
+    print()
+    jumble = ''
+print("Thanks for playing!")
